ff26020277c651c1ff967c14c1bc8e8afc5ab69e
MagnesiumSail <leapster16@gmail.com>
2025-07-07 15:11:22 -0600

    idek changes to schema

---
+++ prisma/migrations/20250706041715_custom_order_tweaks2/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `customNotes` on the `Order` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Order" DROP COLUMN "customNotes";
